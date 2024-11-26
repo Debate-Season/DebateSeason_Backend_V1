75da84d37dfe2e39f4dce8c1d753749d44130c97
Ramyunman <100078669+Ramyunman@users.noreply.github.com>
2024-11-26 20:05:06 +0900

    chat room V1-4

--- src/main/java/com/debateseason_backend_v1/DebateSeasonBackendV1Application.java
+++ src/main/java/com/debateseason_backend_v1/DebateSeasonBackendV1Application.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DebateSeasonBackendV1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DebateSeasonBackendV1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DebateSeasonBackendV1Application.class, args);
+	}
 
 }

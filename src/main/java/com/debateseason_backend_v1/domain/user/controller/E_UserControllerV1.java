d8d77af9693bd9a7a61a605028801c1048984496
KING_ <89888075+kkssbbb@users.noreply.github.com>
2024-11-27 15:43:46 +0900

    Revert "Feature/chat room"

---
+++ src/main/java/com/debateseason_backend_v1/domain/user/controller/E_UserControllerV1.java
@@ -0,0 +1,4 @@
+package com.debateseason_backend_v1.domain.user.controller;
+
+public class E_UserControllerV1 {
+}
---
+++ src/main/java/com/debateseason_backend_v1/domain/user/servcie/E_UserServiceV1.java
@@ -0,0 +1,4 @@
+package com.debateseason_backend_v1.domain.user.servcie;
+
+public class E_UserServiceV1 {
+}

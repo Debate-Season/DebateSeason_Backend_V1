035ec6ea269ca0244bbbc25eb82a05597ab348b1
natty0337 <03337@naver.com>
2024-11-28 14:13:48 +0900

    hotFix: 시큐리티기본 로그인창 리다이렉트 설정
    기존 시큐리티 로그인차이 자동으로 리다이렉트 되지않고 403 응답을 리턴하여 시큐리티 설정에서 시큐리티 기본 로그인창 자동 리턴하도록 설정

--- src/main/java/com/debateseason_backend_v1/config/WebSecurityConfig.java
+++ src/main/java/com/debateseason_backend_v1/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,11 +15,12 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/ws-stomp/**").permitAll()
+                        .requestMatchers("/ws-stomp/**","/login/**","/").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
         return http.build();
     }
 

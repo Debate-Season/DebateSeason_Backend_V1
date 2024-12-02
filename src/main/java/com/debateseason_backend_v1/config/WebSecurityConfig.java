5ba02c169d76de55aa93902e24f4217729b33d78
jongin <jonginwon1203@gmail.com>
2024-12-02 16:20:06 +0900

    feat: 로그인 필터 추가

--- src/main/java/com/debateseason_backend_v1/config/WebSecurityConfig.java
+++ src/main/java/com/debateseason_backend_v1/config/WebSecurityConfig.java
@@ -2,20 +2,35 @@ package com.debateseason_backend_v1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.debateseason_backend_v1.security.jwt.LoginFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+	private final AuthenticationConfiguration authenticationConfiguration;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 
 	@Bean
@@ -30,6 +45,10 @@ public class WebSecurityConfig {
 					.anyRequest().permitAll()
 				// .requestMatchers("/swagger-ui/**", "/actuator/**", "/ws-stomp/**", "/login/**", "/").permitAll()
 				// .anyRequest().authenticated()
+			)
+			.addFilterAt(
+				new LoginFilter(authenticationManager(authenticationConfiguration)),
+				UsernamePasswordAuthenticationFilter.class
 			);
 		return http.build();
 	}

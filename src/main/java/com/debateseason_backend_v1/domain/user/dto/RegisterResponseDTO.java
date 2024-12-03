31426e5fed401e0d27a6d283ec0f71041fa80248
jongin <jonginwon1203@gmail.com>
2024-12-03 20:31:10 +0900

    refactor: 회원 가입 컨트롤러 응답 리팩토링

---
+++ src/main/java/com/debateseason_backend_v1/domain/user/dto/RegisterResponseDTO.java
@@ -0,0 +1,11 @@
+package com.debateseason_backend_v1.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RegisterResponseDTO {
+
+	private Long userId;
+}

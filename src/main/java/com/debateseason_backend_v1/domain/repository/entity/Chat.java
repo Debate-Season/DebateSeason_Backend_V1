3e85a81d64cd1948375f7d99e17054311085b794
jongin <jonginwon1203@gmail.com>
2024-12-11 13:06:08 +0900

    infra: 엔티티와 데이터베이스 쿼리 수정

--- src/main/java/com/debateseason_backend_v1/domain/repository/entity/Chat.java
+++ src/main/java/com/debateseason_backend_v1/domain/repository/entity/Chat.java
@@ -1,7 +1,18 @@
 package com.debateseason_backend_v1.domain.repository.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -10,16 +21,18 @@ import lombok.*;
 @Entity
 public class Chat {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "chat_id")
+	private Long id;
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+	@ManyToOne
+	@JoinColumn(name = "chat_room_id")
+	private ChatRoom chatRoom;
 
-    @ManyToOne
-    private ChatRoom chatRoom;
-
-    // 발신자
-    private String sender;
-    // 소속 커뮤니티
-    private String category;
-    private String content;
+	// 발신자
+	private String sender;
+	// 소속 커뮤니티
+	private String category;
+	private String content;
 }

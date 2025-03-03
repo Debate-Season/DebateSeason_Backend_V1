abc8148f68faeb7684804d57c280298a3c49c871
jongin <jonginwon1203@gmail.com>
2025-03-03 15:16:03 +0900

    refactor: validator 글로벌 예외 처리 설정 수정

--- src/main/java/com/debateseason_backend_v1/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/debateseason_backend_v1/common/exception/GlobalExceptionHandler.java
@@ -47,15 +47,12 @@ public class GlobalExceptionHandler {
 
 		log.error("Validation error occurred", e);
 
-		FieldError fieldError = e.getBindingResult().getFieldError();
-		ErrorCode errorCode = determineErrorCode(fieldError);
-
 		// message detail 생성
 		String detailMessage = createDetailMessage(e.getBindingResult().getFieldErrors());
 
-		ErrorResponse errorResponse = ErrorResponse.of(errorCode, detailMessage);
+		ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.BAD_REQUEST, detailMessage);
 
-		return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
+		return new ResponseEntity<>(errorResponse, ErrorCode.BAD_REQUEST.getHttpStatus());
 	}
 
 	private ResponseEntity<ErrorResponse> createErrorResponseEntity(ErrorCode errorCode) {
@@ -63,31 +60,6 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(
 			ErrorResponse.of(errorCode),
 			errorCode.getHttpStatus());
-	}
-
-	// validation 오류에 대한 ErrorCode 결정
-	private ErrorCode determineErrorCode(FieldError fieldError) {
-
-		if (fieldError == null) {
-			return ErrorCode.INVALID_INPUT_VALUE;
-		}
-
-		String code = fieldError.getCode();
-		switch (code) {
-			case "NotBlank":
-			case "NotNull":
-			case "NotEmpty":
-				return ErrorCode.MISSING_REQUIRED_VALUE;
-			case "Pattern":
-			case "Email":
-				return ErrorCode.INVALID_FORMAT;
-			case "Min":
-			case "Max":
-			case "Size":
-				return ErrorCode.VALUE_OUT_OF_RANGE;
-			default:
-				return ErrorCode.INVALID_INPUT_VALUE;
-		}
 	}
 
 	private String createDetailMessage(List<FieldError> fieldErrors) {

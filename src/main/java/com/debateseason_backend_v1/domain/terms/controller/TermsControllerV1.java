bdbb5fcd1f71c334ebe09d265ac365fcd83b29fb
jongin <jonginwon1203@gmail.com>
2025-03-17 18:41:56 +0900

    feat: 약관 동의 기능 구현

--- src/main/java/com/debateseason_backend_v1/domain/terms/controller/TermsControllerV1.java
+++ src/main/java/com/debateseason_backend_v1/domain/terms/controller/TermsControllerV1.java
@@ -2,16 +2,24 @@ package com.debateseason_backend_v1.domain.terms.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.debateseason_backend_v1.common.response.ApiResult;
+import com.debateseason_backend_v1.common.response.VoidApiResult;
+import com.debateseason_backend_v1.domain.terms.controller.request.TermsAgreementRequest;
 import com.debateseason_backend_v1.domain.terms.service.TermsServiceV1;
 import com.debateseason_backend_v1.domain.terms.service.response.LatestTermsResponse;
+import com.debateseason_backend_v1.security.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/terms")
@@ -34,4 +42,16 @@ public class TermsControllerV1 {
 
 	// TODO: 약관 동의 API
 
+	@PostMapping("/agree")
+	public VoidApiResult agree(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@RequestBody TermsAgreementRequest request
+	) {
+
+		log.info("request = {}", request.agreements().toString());
+
+		termsService.agree(request.toServiceRequest(userDetails.getUserId()));
+
+		return VoidApiResult.success("약관 동의에 성공했습니다.");
+	}
 }
--- src/main/java/com/debateseason_backend_v1/domain/terms/controller/request/TermsAgreementRequest.java
+++ src/main/java/com/debateseason_backend_v1/domain/terms/controller/request/TermsAgreementRequest.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 import com.debateseason_backend_v1.domain.terms.service.request.TermsAgreementServiceRequest;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(title = "이용약관 동의 요청 DTO", description = "이용약관 동의 요청")
 public record TermsAgreementRequest(
-	@Schema(description = "이용약관 동의 목록 DTO")
 	List<TermsAgreementItem> agreements
 ) {
 

16d23fb8c9f10f8499d7cefe351e70bf12000bd4
kkssbbb <03337@naver.com>
2025-07-02 13:23:46 +0900

    test: 피처패턴 chat,chatroom,issue 생성

--- src/test/java/com/debateseason_backend_v1/fixtures/chat/ChatFixture.java
+++ src/test/java/com/debateseason_backend_v1/fixtures/chat/ChatFixture.java
@@ -10,14 +10,10 @@ import java.time.LocalDateTime;
 public class ChatFixture {
 
     public static Chat create() {
-        return ChatFixture.createWithUserId(1L);
-    }
-
-    public static Chat createWithUserId(Long userId) {
         return Chat.builder()
                 .id(1L)
                 .chatRoomId(ChatRoomFixture.create())
-                .userId(userId)
+                .userId(1L)
                 .messageType(MessageType.CHAT)
                 .content("content")
                 .sender("sender")
@@ -26,22 +22,4 @@ public class ChatFixture {
                 .timeStamp(LocalDateTime.now())
                 .build();
     }
-
-    /**
-     * 신고된 메시지 테스트용
-     */
-    public static Chat createMaskReportedChat(Chat chat) {
-        return Chat.builder()
-                .id(chat.getId())
-                .chatRoomId(chat.getChatRoomId())
-                .userId(chat.getUserId())
-                .messageType(chat.getMessageType())
-                .content(Chat.REPORTED_MESSAGE_CONTENT)
-                .sender(chat.getSender())
-                .opinionType(chat.getOpinionType())
-                .userCommunity(chat.getUserCommunity())
-                .timeStamp(chat.getTimeStamp())
-                .build();
-    }
-
 }
